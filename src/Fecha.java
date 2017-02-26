33e98007302cf1e060787f5fd7c27bdb40150af7
RoberWork <RoberWork@Rober-PC>
2017-02-26 20:39:44 +0100

    commit1

--- src/Fecha.java
+++ src/Fecha.java
@@ -48,21 +48,7 @@ public class Fecha {
 		}
  
 
-public static void main(String[] args)throws IOException {
-int dia, mes, anyo;
-System.out.println ("Introduce un día: ");
-BufferedReader entrada = new BufferedReader(new InputStreamReader (System.in));
-dia = Integer.parseInt(entrada.readLine());
-System.out.println ("Introduce un mes: ");
-mes = Integer.parseInt(entrada.readLine());
-System.out.println ("Introduce un año: ");
-anyo = Integer.parseInt(entrada.readLine());
-Fecha f1=new Fecha(dia,mes,anyo);
-if (f1.valida())
-System.out.println("La fecha: "+dia+"/"+mes+"/"+anyo+" esválida");
-else
-System.out.println("La fecha: "+dia+"/"+mes+"/"+anyo+"NO es válida");
-}
+
 }
 
 
